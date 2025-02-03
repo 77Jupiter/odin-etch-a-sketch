e3c610db16408ccdd5434683a536318ceed53121
Cameron Brown <moontsukuyomi6@gmail.com>
2025-02-03 16:33:58 +0200

    Create button to update grid size on user input

--- javascript.js
+++ javascript.js
@@ -1,8 +1,29 @@
 const gridContainer = document.querySelector('.grid-container');
 
+let gridSize = 256;
+
+function updateGridSize() {
+  const gridButtonElement = document.querySelector('.js-grid-button');
+  gridButtonElement.addEventListener('click', () => {
+    let newSize = prompt('Insert a grid size');
+    newSize = parseInt(newSize);
+    console.log(newSize);
+    if (newSize > 0) {
+      gridSize = newSize * newSize;
+      gridContainer.style.height = (52 * newSize) + 'px';
+      gridContainer.style.width = (52 * newSize) + 'px';
+      createDiv();
+    } else {
+      alert('Please enter a valid number');
+    }
+  })
+}
+
+updateGridSize();
+
 function createDiv() {
   gridContainer.innerHTML = '';
-  for (i = 0; i < 256; i++) {
+  for (i = 0; i < gridSize; i++) {
     const div = document.createElement('div');
     div.classList.add('grid-item');
     gridContainer.appendChild(div);
