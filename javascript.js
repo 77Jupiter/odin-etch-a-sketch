184de7d6b80afc33674d921edbf859dc944c953c
Cameron Brown <moontsukuyomi6@gmail.com>
2025-02-03 15:49:18 +0200

    Create etch-a-sketch grid layout

--- javascript.js
+++ javascript.js
@@ -1,11 +1,16 @@
 const gridContainer = document.querySelector('.grid-container');
-
 function createDiv() {
   for (i = 0; i < 256; i++) {
     const div = document.createElement('div');
     div.classList.add('grid-item');
     gridContainer.appendChild(div);
   }
+  const gridItem = document.querySelectorAll('.grid-item');
+  gridItem.forEach((item) => {
+    item.addEventListener('mouseover', () => {
+      item.style.backgroundColor = 'black';
+    });
+  });
 }
 
 createDiv();
